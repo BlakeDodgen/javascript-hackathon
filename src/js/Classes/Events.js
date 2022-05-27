512a9829c36cad131f3f3de7476bc59e9d789bf1
Blake Dodgen <blakedodgen@gmail.com>
2022-05-27 14:57:33 +0200

    stuff

--- src/js/Classes/Events.js
+++ src/js/Classes/Events.js
@@ -1,56 +1,46 @@
-// document.querySelector('.container')
-var buttons = document.querySelectorAll('.button');
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', buttonPress);
-}
-function buttonPress(event) {
-    console.log(buttons[i])
-}
-
-
+const url = 'https://test-api.codingbootcamp.cz/api/ee1ff8bd/events';
+let buttons = document.querySelectorAll('.button');
 
 class Event {
-    constructor(name, image, description, button) {
-        this.name = name;
-        this.image = image;
-        this.description = description;
-        this.button = button;
-        this.createEvent()
-    }
-    createEvent() {
-        let container = document.querySelector('.event')
-        // console.log(container)
-        const box = document.createElement('div')
-        box.classList.add('event__box')
-        box.innerHTML += `
-                <div class="event__name">${this.name}</div>
-                <button class="event__button">${this.button}</button> 
-                </div>
-        `
-        // do more stuff, like assign event listeners
-        container.appendChild(box)
-    }
-}
+        constructor(name, image, description, button) {
+                this.name = name;
+                this.image = image;
+                this.description = description;
+                this.button = button;
+                this.getAPI();
+            }
 
-
-// const events = [
-//     new Event('AA', '', 'blabla', 'button'),
-//     new Event('AAbb', '', 'blabla', 'button'),
-//     new Event('AAbbcc', '', 'blabla', 'button'),
-//     new Event('AAccddss', '', 'blabla', 'button'),
-//     new Event('AAdddssffgh', '', 'blabla', 'button'),
-// ]
-
-
-
-export { Event }
-
-
-{/* <div class="feature">
-<div class="feature__event">${this.name}</div>
-<div class="feature__img">${this.image}</div>
-<div class="feature__name">${this.name}</div>
-<div class="feature__description">${this.description}</div>
-<button class="button">${this.button}</button>
-
-</div> */}
+            getAPI = async () => {
+                const response = await fetch(url);
+                const data = await response.json();
+                
+                for (let i = 0; i < data.length; i++) {
+                    let eventName = data[i].name;
+                    let eventDescription = data[i].description;
+                    let eventImage = data[i].image_url;
+                    
+                    const createEvent = () => {
+                        let container = document.querySelector('.event')
+                        // console.log(container)
+                        const box = document.createElement('div')
+                        box.classList.add('event__box')
+                        box.innerHTML += `
+                        <div class="event__name">${eventName}</div>
+                        <button class="event__button">${this.button}</button> 
+                        </div>
+                        `
+                        // do more stuff, like assign event listeners
+                        container.appendChild(box)
+                    }
+                    createEvent()
+                } 
+            }
+        } 
+            
+            export { Event }
+            // buttonPress() {
+                // for (let i = 0; i < buttons.length; i++) {
+            //     buttons[i].addEventListener('click', buttonPress);
+             // }
+            //     console.log(buttons[i])
+            // }
