4a5c0ad6f3cb88f7c1fa4c27fe4c28a65b95edc1
Blake Dodgen <blakedodgen@gmail.com>
2022-05-27 11:05:41 +0200

    buttons array

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,19 +1,24 @@
-var buttons = document.querySelectorAll('.js-say-hi');
-var displayBox = document.querySelector('.js-display-greeting');
+var buttons = document.querySelectorAll('.button');
+//var displayBox = document.querySelector('.event');
 
 // wire displayGreeting function to each button with .js-say-hi class
-for (var i=0; i<buttons.length; i++) {
-  buttons[i].addEventListener('click', displayGreeting)
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', buttonPress);
 }
 
-function displayGreeting(event) {
+function buttonPress(event) {
   var buttonNumber = event.target.textContent[event.target.textContent.length - 1];
   displayBox.textContent += 'hello from button ' + buttonNumber + '! ';  
 }
 
-{/* <button class="js-say-hi">Say hi 1</button>
+/* <button class=".button">Say hi 1</button>
 <button class="js-say-hi">Say hi 2</button>
 <button class="js-say-hi">Say hi 3</button>
 <button class="js-say-hi">Say hi 4</button>
 <button class="js-say-hi">Say hi 5</button>
-<div class="js-display-greeting"></div> */}
+<div class="js-display-greeting"></div> */
+
+export const buttonPress = () => {
+    
+};
+
